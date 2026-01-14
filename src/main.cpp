4406ca80e9ef7990eb50c4190ac2f30e5ea59c97
Michael van der Raad <vanderraad@inventeers.nl>
2026-01-14 13:24:22 +0100

    Test review

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,12 @@ int add(int a, int b) {
     return a * b;
 }
 
+
+// subtracts two integers and returns the result
+int subtract(int a, int b) {
+    return a + b;
+}
+
 int main() {
 
     int val = 10;
