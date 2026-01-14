439db105c9ecfe0963f0f4ea54faf7b6894b155e
Michael van der Raad <vanderraad@inventeers.nl>
2026-01-14 12:30:31 +0100

    Add new string bug

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,12 @@ int main() {
 
     int res = 10 / add(5, 5);
 
+    char name[10];
+
+    for (int i = 0; i < 10; i++) {
+        name[i] = 'A';
+    }
+
     std::cout << "The result of addition is: " << result << std::endl;
     return 0;
 }
