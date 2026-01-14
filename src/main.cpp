94c6fd203eb04ff8cbb42b691b0e5a25489b3d2d
Michael van der Raad <vanderraad@inventeers.nl>
2026-01-14 12:25:09 +0100

    Fix main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 // Adds two integers and returns the result
 int add(int a, int b) {
-    return a * b;
+    return a - b;
 }
 
 int main() {
@@ -12,6 +12,9 @@ int main() {
     int val2 = val;
 
     int result = add(3, 4);
+
+    int res = 10 / add(5, 5);
+
     std::cout << "The result of addition is: " << result << std::endl;
     return 0;
 }
